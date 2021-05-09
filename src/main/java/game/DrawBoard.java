6d0e0b766f9880755a318234a011d4cd4506037a
Konn25 <koncz.bence9@gmail.com>
2021-05-09 21:46:58 +0200

    Edit playerWin function

--- src/main/java/game/DrawBoard.java
+++ src/main/java/game/DrawBoard.java
@@ -1,8 +1,6 @@
 package game;
 
-import game.model.Draw;
-import game.model.MoveCheck;
-import game.model.Piece;
+import game.model.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,11 +15,15 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.statement.Slf4JSqlLogger;
 import org.tinylog.Logger;
 
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DrawBoard {
 
@@ -50,7 +52,7 @@ public class DrawBoard {
     boolean gameEnd=false;
 
     private String winner2="";
-
+    public Jdbi scoreBoard;
 
     @FXML
     Label redNameLabel,blueNameLabel;
@@ -69,6 +71,10 @@ public class DrawBoard {
 
     @FXML
     public void initialize(){
+
+        System.out.println(scoreBoard+"----------------------------------------------------------------");
+
+
         endGame.setVisible(false);
         endText.setVisible(false);
         grid= new Rectangle[4][5];
@@ -138,7 +144,7 @@ public class DrawBoard {
 
     }
 
-     public void checkValues(int [][] adat){
+    public void checkValues(int [][] adat){
         int color=0; //1--kék, 2--piros
         int blueColor=1;
         int redColor=2;
@@ -179,7 +185,7 @@ public class DrawBoard {
         lastY=(int)p.getY() / squareSizeY;
         System.out.println(lastX+" "+lastY);
 
-        if(c.getFill() == Color.RED && whichPlayerMove==false) {
+        if(c.getFill() == Color.RED && !whichPlayerMove) {
             MoveCheck.checkPossibleMove(lastX, lastY, adat, grid);
             Logger.info("A tábla jelenlegi állapota\n"+ adat[0][0]+" "+adat[0][1]+" "+adat[0][2]+" "+adat[0][3]+"\n"+adat[1][0]+" "+adat[1][1]+" "+adat[1][2]+" "+adat[1][3]+"\n"+
                     adat[2][0]+" "+adat[2][1]+" "+adat[2][2]+" "+adat[2][3]+"\n"+adat[3][0]+" "+adat[3][1]+" "+adat[3][2]+" "+adat[3][3]+"\n"+
@@ -201,7 +207,7 @@ public class DrawBoard {
 
     public void dragged(MouseEvent event, Piece p, Circle c, boolean whichPlayerMove){
 
-        if(c.getFill() == Color.RED && whichPlayerMove==false){
+        if(c.getFill() == Color.RED && !whichPlayerMove){
             p.setX(p.getX()+event.getX());
             p.setY(p.getY()+event.getY());
             p.draw();
@@ -226,11 +232,11 @@ public class DrawBoard {
         int blueNumber=1;
 
         try{
-            if(c.getFill()==Color.RED && whichPlayerMove==false && gameEnd == false){
+            if(c.getFill()==Color.RED && !whichPlayerMove&& !gameEnd){
 
                 whichPlayerMove=MoveCheck.movePiece(p,adat,gridx,gridy,squareSizeX,squareSizeY,lastX,lastY,redNumber,false);
                 MoveCheck.movePiece(p,adat,gridx,gridy,squareSizeX,squareSizeY,lastX,lastY,redNumber,whichPlayerMove);
-            }else if(c.getFill()==Color.BLUE && whichPlayerMove==true && gameEnd == false){
+            }else if(c.getFill()==Color.BLUE && whichPlayerMove && !gameEnd){
 
                 whichPlayerMove=MoveCheck.movePiece(p,adat,gridx,gridy,squareSizeX,squareSizeY,lastX,lastY,blueNumber,true);
                 MoveCheck.movePiece(p,adat,gridx,gridy,squareSizeX,squareSizeY,lastX,lastY,blueNumber,whichPlayerMove);
@@ -241,9 +247,9 @@ public class DrawBoard {
                 Logger.info("Vissza rakom mert nem jó a lépés");
             }
 
-            if(whichPlayerMove==false){
+            if(!whichPlayerMove){
                 moveText.setText("Piros játékos jön");
-            }else if(whichPlayerMove){
+            }else{
                 moveText.setText("Kék játékos jön");
             }
 
@@ -274,10 +280,21 @@ public class DrawBoard {
         end.setWinner(winner2);
         end.getPlayer(redNameLabel.getText(),blueNameLabel.getText());
 
+        List<DataModel> dm;
+        try (Handle handle = RegPlayerControl.jd.open()){
+            DataBase.createTabel(RegPlayerControl.jd);
+            DataBase.uploadTabelTestElement(handle);
+            dm = DataBase.getScoreBoard(handle);
+        }
+        end.setScoreBoard(dm);
+
+
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         stage.setTitle("Játék vége");
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
+
+
 }
