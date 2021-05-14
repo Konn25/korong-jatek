c3361985a8cafd18cd2612d3523464e83b189f5d
Konn25 <koncz.bence9@gmail.com>
2021-05-14 13:21:43 +0200

    Adedd comment, and change DrawTable name to DrawBoard

--- src/main/java/game/model/Draw.java
+++ src/main/java/game/model/Draw.java
@@ -1,33 +1,27 @@
 package game.model;
 
-import game.DrawBoard;
-import javafx.fxml.FXML;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
 import org.tinylog.Logger;
 
-import javafx.scene.input.MouseEvent;
-import java.util.ArrayList;
+
+/**
+ * Draw the board
+ */
 
 public class Draw {
 
-    /*
-        *Draw the tabel
-        * grind: store the table rows and colums
-        * size: table size in px
-        * squareSizeX: how many squres in the X axis
-        * squarSizeY: how many squers  in the Y axis
-        * pane: FXML Pane
+    /**
+     * Draw the board
+     * @param grid store the grid position
+     * @param size board size
+     * @param squareSizeX X axis square number
+     * @param squareSizeY Y axis square number
+     * @param pane a pane where the board draw
      */
 
-    public static void DrawTable(Rectangle[][] grid, int size, int squareSizeX, int squareSizeY, Pane pane) {
+    public static void DrawBoard(Rectangle[][] grid, int size, int squareSizeX, int squareSizeY, Pane pane) {
         for (int i = 0; i < size; i += squareSizeX) {
             for (int j = 0; j < size; j += squareSizeY) {
                 Rectangle r = new Rectangle(i, j, squareSizeX, squareSizeY);
