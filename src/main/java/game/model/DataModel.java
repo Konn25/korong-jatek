a5ce54d1e04de75bd04df4ed3044d8ab7afee10f
Konn25 <koncz.bence9@gmail.com>
2021-05-14 13:21:58 +0200

    Deleted comment

--- src/main/java/game/model/DataModel.java
+++ src/main/java/game/model/DataModel.java
@@ -4,24 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class DataModel {
-
-    /*
-        Player one is the red player
-     */
     private String playerone;
 
-    /*
-        Player two is the blue player
-     */
     private String playertwo;
 
-    /*
-        Player who win the game
-     */
     private String winner;
 
 }
