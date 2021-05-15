9387e39a08669a86e28a26b7c15b945f12e6a0c7
Konn25 <koncz.bence9@gmail.com>
2021-05-15 17:42:23 +0200

    Code formatting

--- src/main/java/game/model/Piece.java
+++ src/main/java/game/model/Piece.java
@@ -12,49 +12,53 @@ public class Piece {
     private double radius;
     private Circle c;
 
-    public Piece(double x, double y, double radius, Circle c){
-        this.x=x;
-        this.y=y;
-        this.radius=radius;
-        this.c=c;
+    public Piece(double x, double y, double radius, Circle c) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.c = c;
     }
 
     /**
-     *  Set piece Y axis
+     * Set piece Y axis
+     *
      * @param y the piece y axis position
      */
-    public void setY(double y){
-        this.y=y;
+    public void setY(double y) {
+        this.y = y;
     }
 
     /**
      * Set piece X
+     *
      * @param x he piece x axis position
      */
-    public void setX(double x){
-        this.x=x;
+    public void setX(double x) {
+        this.x = x;
     }
 
     /**
      * Get piece X position
+     *
      * @return piece X position
      */
-    public double getX(){
+    public double getX() {
         return x;
     }
 
     /**
      * Get piece Y position
+     *
      * @return piece Y position
      */
-    public double getY(){
+    public double getY() {
         return y;
     }
 
     /**
      * Draw piece
      */
-    public void draw(){
+    public void draw() {
         c.setRadius(radius);
         c.setTranslateX(x);
         c.setTranslateY(y);
