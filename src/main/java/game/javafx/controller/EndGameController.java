9d4dbc70002172a3e410cce2ccdeb88708116214
Konn25 <koncz.bence9@gmail.com>
2021-05-15 17:40:51 +0200

    Code formatting

--- src/main/java/game/javafx/controller/EndGameController.java
+++ src/main/java/game/javafx/controller/EndGameController.java
@@ -1,6 +1,6 @@
-package game.control;
+package game.javafx.controller;
 
-import game.model.DataModel;
+import game.results.DataModel;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -19,7 +19,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.List;
 
-public class EndGameControl {
+public class EndGameController {
 
     @FXML
     Label winnerLabel;
@@ -40,24 +40,24 @@ public class EndGameControl {
     private Scene scene;
     private Parent root;
 
-    private String bluePlayer="";
-    private String redPlayer="";
+    private String bluePlayer = "";
+    private String redPlayer = "";
 
     private FXMLLoader loader;
 
-    public void setWinner(String win){
-        winnerLabel.setText("Winner: "+win);
+    public void setWinner(String win) {
+        winnerLabel.setText("Winner: " + win);
     }
 
-    public void getPlayers(String red, String blue){
-        redPlayer=red;
-        bluePlayer=blue;
+    public void getPlayers(String red, String blue) {
+        redPlayer = red;
+        bluePlayer = blue;
     }
 
-    public void setScoreBoard(List<DataModel> list){
+    public void setScoreBoard(List<DataModel> list) {
 
-        ObservableList<DataModel>dats = FXCollections.observableArrayList();
-        
+        ObservableList<DataModel> dats = FXCollections.observableArrayList();
+
         RedPlayerCol.setCellValueFactory(new PropertyValueFactory<DataModel, String>("playerone"));
         BluePlayerCol.setCellValueFactory(new PropertyValueFactory<DataModel, String>("playertwo"));
         WinnerCol.setCellValueFactory(new PropertyValueFactory<DataModel, String>("winner"));
@@ -75,7 +75,7 @@ public class EndGameControl {
         loader = new FXMLLoader(getClass().getResource("/regplayer.fxml"));
         root = loader.load();
 
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setTitle("Új játék");
         stage.setScene(scene);
@@ -83,19 +83,19 @@ public class EndGameControl {
     }
 
 
-    public void reMatch(ActionEvent event) throws IOException{
+    public void reMatch(ActionEvent event) throws IOException {
 
         loader = new FXMLLoader(getClass().getResource("/game.fxml"));
         root = loader.load();
-        DrawBoard dr = loader.getController();
+        DrawBoardController dr = loader.getController();
         dr.setBlueName(bluePlayer);
         dr.setRedName(redPlayer);
 
 
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
-        stage.setTitle("Red player: "+redPlayer+"  Blue player: "+bluePlayer);
+        stage.setTitle("Red player: " + redPlayer + "  Blue player: " + bluePlayer);
         stage.show();
     }
 
